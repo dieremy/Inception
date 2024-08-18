6131ba280ab6c36186d313dcd0883027a80a5409
dieremy <rrg@gmail.com>
2024-08-18 13:40:51 -0400

    still to fix

--- srcs/containers/mariadb/script.sh
+++ srcs/containers/mariadb/script.sh
@@ -1,34 +1,37 @@
-#!bin/sh
+#!/bin/bash
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+kill $(cat /var/run/mysqld/mariadb.pid)
+# rc-service mariadb restart
+mysqld
+# mysqld_safe --datadir="/var/lib/mysql"
 
-  chown -R mysql:mysql /var/lib/mysql
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+#   chown -R mysql:mysql /var/lib/mysql
 
-  # init database
-  mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+#   # Init database
+#   mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-  tfile=`mktemp`
-  if [ ! -f "$tfile" ]; then
-    return 1
-  fi
-fi
+#   tfile=$(mktemp)
+#   if [ ! -f "$tfile" ]; then
+#     return 1
+#   fi
+# fi
 
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+# if [ ! -d "/var/lib/mysql/wordpress" ]; then
+# cat << EOF > /tmp/create_db.sql
+# USE mysql;
+# FLUSH PRIVILEGES;
+# DELETE FROM mysql.user WHERE User='';
+# DROP DATABASE test;
+# DELETE FROM mysql.db WHERE Db='test';
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+# CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+# CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+# FLUSH PRIVILEGES;
+# EOF
 
-cat << EOF > /tmp/create_db.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PSW}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-  # run init.sql
-  /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-  rm -f /tmp/create_db.sql
-fi
+#   /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+#   rm -f /tmp/create_db.sql
+# fi
--- srcs/containers/wordpress/script.sh
+++ srcs/containers/wordpress/script.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-while ! mysql -hmariadb -u$DB_USER -p$DB_PSW &>/dev/null; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 3
-done
+# while ! mysql -hmariadb -u$DB_USER -p$DB_PSW &>/dev/null; do
+#     echo "Waiting for MariaDB to be ready..."
+#     sleep 3
+# done
 
-if [ ! -f "/var/www/wp-config.php" ]; then
-    echo "Starting wp set up..."
-    wp core download --allow-root
-    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PSW --dbhost=mariadb --allow-root
-    wp core install --url=$DOMAIN_NAME --title="Sdesdo's Site" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --skip-email --allow-root
-    wp user create sdesdo sdesdo@mail.com --role=author --user_pass=Secret1! --allow-root
-    wp theme install bizboost --activate --allow-root
-fi
+# if [ ! -f "/var/www/html/wp-config.php" ]; then
+cd /var/www/html/
 
-/usr/sbin/php-fpm8 -F
+echo "Starting WordPress setup..."
+wp core download --allow-root
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb --allow-root
+wp core install --url=$DOMAIN_NAME --title="Your Site" --admin_user=admin --admin_password=admin --admin_email=admin@example.com --skip-email --allow-root
+wp user create sdesdo sdesdo@mail.com --role=author --user_pass=Secret1! --allow-root
+wp theme install bizboost --activate --allow-root
+wp-cli plugin install wordpress-importer --activate
+wp-cli import wordpress-inception.xml --authors=create
+# fi
+
+/usr/sbin/php-fpm82 -F
